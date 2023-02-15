adbadc9cfbb9ca4268b41b8dc6b65efdff03653f
Aan Nea <90422412+aannea@users.noreply.github.com>
2023-02-15 11:47:16 +0700

    Type Data

--- Dasar.py
+++ Dasar.py
@@ -1,2 +1,26 @@
-world = "world"
-print('Hello', world)
+#Output terminal
+print('Hello world')
+
+#Variable
+#Integer
+nilaiAwal = 0
+nilaiAkhir = 10
+#Float
+nilaiFloat = 1.5
+#String
+sebuahString = "Kata"
+#Boolean
+nilaiBoolean = True
+#Type Data Khusus
+dataComplex = complex(5,6)
+
+print("Nilai Awal :", nilaiAwal)
+print("Nilai Akhir :", nilaiAkhir)
+print("Dijumlahkan : ", nilaiAwal + nilaiAkhir)
+
+#Type
+print("Type dari nilai Awal :", type(nilaiAwal))
+print("Type dari nilai Float :", type(nilaiFloat))
+print("Type dari sebuah String :", type(sebuahString))
+print("Type dari nilai boolean :", type(nilaiBoolean))
+print("Type dari data Complex :", type(dataComplex))
