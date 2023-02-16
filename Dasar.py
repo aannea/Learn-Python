cdbd5b74a52f0411683459a6961cbdc81e2af33b
Aan Nea <90422412+aannea@users.noreply.github.com>
2023-02-16 23:05:14 +0700

    Casting

--- Dasar.py
+++ Dasar.py
@@ -24,3 +24,14 @@ print("Type dari nilai Float :", type(nilaiFloat))
 print("Type dari sebuah String :", type(sebuahString))
 print("Type dari nilai boolean :", type(nilaiBoolean))
 print("Type dari data Complex :", type(dataComplex))
+
+#Casting(merubah dari satu type ke type lain)
+data_int = 9;
+print(f"data = {data_int}, type {type(data_int)}")
+#untuk merubah dapat menggunakan operator casting
+data_float = float(data_int)
+print(f"data = {data_float}, type {type(data_float)}")
+data_bool = bool(data_int)
+print(f"data = {data_bool}, type {type(data_bool)}")
+data_str = str(data_int)
+print(f"data = {data_str}, type {type(data_str)}")
