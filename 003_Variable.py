5b4862a5522442a1591966e85447d1f462fb30d2
Aan Nea <90422412+aannea@users.noreply.github.com>
2023-07-12 21:48:47 +0700

    Create 003_Variable.py

---
+++ 003_Variable.py
@@ -0,0 +1,15 @@
+#Integer
+nilaiAwal = 0
+nilaiAkhir = 10
+#Float
+nilaiFloat = 1.5
+#String
+sebuahString = "Kata"
+#Boolean
+nilaiBoolean = True
+#Type Data Khusus
+dataComplex = complex(5,6)
+
+print("Nilai Awal :", nilaiAwal)
+print("Nilai Akhir :", nilaiAkhir)
+print("Dijumlahkan : ", nilaiAwal + nilaiAkhir)
